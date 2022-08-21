fb811ccdc8158c3d642ac3813701deabeaf71943
han yujeong <yuja0529@gmail.com>
2022-08-22 04:26:14 +0900

    투두리스트 제작 첫날

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,12 @@
   import IndexSeachBox from "~/components/indexSeachBox.svelte";
 
   function mainTodoAdd (event){
-    console.log(event.detail.text)
+    console.log(event.detail.text);
   }
 </script>
 
 <IndexHeader/>
+
 <main class="p(0/20) hbox">
   <IndexSeachBox on:sayTodo={mainTodoAdd}/>
 </main>
--- src/components/indexHeader.svelte
+++ src/components/indexHeader.svelte
@@ -1,3 +1,25 @@
-<div class="hbox h(80) bg(#E6F0EF) c(#4B3C2D) p(0/20) font(20) m(0/0/10/0)">
-    <h1>유정이의 투두리스트</h1>
+<script>
+    import { Confetti } from "svelte-confetti"
+    import { tick } from "svelte"
+
+    export let toggleOnce = false;
+
+    let active = false;
+
+    async function click() {
+        if (toggleOnce) {
+            console.log("야");
+            active = !active;
+            return;
+        }
+
+        active = false;
+        await tick();
+        active = true;
+
+    };
+</script>
+<div class="hbox space-between h(80) bg(#E6F0EF) c(#4B3C2D) p(0/20) font(20) m(0/0/10)">
+    <h1>오늘의 할일</h1>
+    <i class="fa-solid fa-carrot c(#4B3C2D)" on:click={click}><Confetti /></i>
 </div>
